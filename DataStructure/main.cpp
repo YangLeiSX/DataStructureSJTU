70b694469be150d07ae25c317d0b72facb393de2
YangLei <791377151@qq.com>
2019-06-29 22:29:01 +0800

    add the data structure red black tree

--- DataStructure/main.cpp
+++ DataStructure/main.cpp
@@ -1,6 +1,6 @@
 //
 //  main.cpp
-//  Stest
+//  DataStructure
 //  用于数据结构的学习和代码实现
 //
 //  Created by 杨磊 on 2019/1/14.
@@ -12,17 +12,7 @@
 #include <cstdlib>
 #include <ctime>
 
-#include <vector>
-#include <list>
-#include <stack>
-#include <queue>
-#include <string>
-#include <map>
-#include <set>
-#include <algorithm>
-#include <utility>
-
-#include "review.h"
+#include "set.hpp"
 
 int main(int argc, const char * argv[]) {
     
--- DataStructure/set.hpp
+++ DataStructure/set.hpp
@@ -642,6 +642,297 @@ void AVLTree<KEY,OTHER>::makeEmpty(AVLNode *t) {
     delete t;
 }
 
+
+/*
+ *********************************************
+ 使用红黑树实现的动态查找表
+ *********************************************
+ */
+template <class KEY, class OTHER>
+class RedBlackTree:public dynamicSearchTable<KEY, OTHER> {
+    enum colorT{RED, BLACK};
+    struct RedBlackNode{
+        SET<KEY, OTHER> data;
+        RedBlackNode *left;
+        RedBlackNode *right;
+        colorT colour;
+        
+        RedBlackNode(const SET<KEY, OTHER> &element, RedBlackNode *lt = NULL, RedBlackNode *rt = NULL, colorT h = RED)
+        :data(element),left(lt),right(rt),colour(h){}
+    };
+    
+    RedBlackNode *root;
+    
+public:
+    RedBlackTree(RedBlackNode *t = NULL){
+        root = t;
+    }
+    ~RedBlackTree(){makeEmpty(root);}
+    SET<KEY, OTHER> *find(const KEY &x)const;
+    void insert(const SET<KEY, OTHER> &x);
+    void remove(const KEY &x);
+    
+private:
+    void makeEmpty(RedBlackNode *&t);
+    void insertAdjust(RedBlackNode *gp, RedBlackNode *p, RedBlackNode *t);
+    void removeAdjust(RedBlackNode *&p, RedBlackNode *&c, RedBlackNode *&t, KEY del);
+    void LL(RedBlackNode *gp);
+    void RR(RedBlackNode *gp);
+    void LR(RedBlackNode *gp);
+    void RL(RedBlackNode *gp);
+};
+
+template <class KEY, class OTHER>
+SET<KEY, OTHER> *RedBlackTree<KEY, OTHER>::find(const KEY &x)const {
+    RedBlackNode *t = root;
+    while(t != NULL && t->data.key != x)
+        if(t->data.key > x)
+            t = t->left;
+        else
+            t = t->right;
+    
+    if(t == NULL)
+        return NULL;
+    else
+        return (SET<KEY, OTHER> *)t;
+}
+
+template <class KEY, class OTHER>
+void RedBlackTree<KEY, OTHER>::makeEmpty(RedBlackNode *&t) {
+    if(t != NULL){
+        makeEmpty(t->left);
+        makeEmpty(t->right);
+        delete t;
+    }
+    t = NULL;
+}
+
+//调整函数
+template <class KEY, class OTHER>
+void RedBlackTree<KEY, OTHER>::LL(RedBlackNode *gp) {
+    RedBlackNode *p = gp->left, *t = p->left;
+    RedBlackNode tmp = *gp;
+    
+    gp->data = p->data;
+    gp->left = t;
+    gp->right = p;
+    p->data = tmp.data;
+    p->left = p->right;
+    p->right = tmp.right;
+}
+
+template <class KEY, class OTHER>
+void RedBlackTree<KEY, OTHER>::LR(RedBlackNode *gp) {
+    RedBlackNode *p = gp->left, *t= p->right;
+    RedBlackNode tmp = *gp;
+    
+    gp->data = t->data;
+    gp->right = t;
+    p->right = t->left;
+    t->data = tmp.data;
+    t->left = t->right;
+    t->right = tmp.right;
+}
+
+template <class KEY, class OTHER>
+void RedBlackTree<KEY, OTHER>::RR(RedBlackNode *gp) {
+    RedBlackNode *p = gp->right, *t = p->right;
+    RedBlackNode tmp = *gp;
+    
+    gp->data = p->data;
+    gp->right = t;
+    gp->left = p;
+    p->data = tmp.data;
+    p->right = p->left;
+    p->left = tmp.left;
+}
+
+template <class KEY, class OTHER>
+void RedBlackTree<KEY, OTHER>::RL(RedBlackNode *gp) {
+    RedBlackNode *p = gp->right, *t = p->left;
+    RedBlackNode tmp = *gp;
+    
+    gp->data = t->data;
+    gp->left = t;
+    p->left = t->right;
+    t->data = tmp.data;
+    t->right = t->left;
+    t->left = tmp.left;
+}
+
+//插入过程
+template <class KEY, class OTHER>
+void RedBlackTree<KEY, OTHER>::insert(const SET<KEY, OTHER> &x) {
+    RedBlackNode *t, *parent, *grandP;
+    
+    if(root == NULL){
+        root = new RedBlackNode(x,NULL,NULL,BLACK);
+        return ;
+    }
+    
+    parent = grandP = t = root;
+    while(true){
+        if(t){
+            if(t->left && t->left->colour == RED && t->right && t->right->colour == RED){
+                t->left->colour = t->right->colour = BLACK;
+                t->colour = RED;
+                insertAdjust(grandP, parent, t);
+            }
+            grandP = parent;
+            parent = t;
+            t = (t->data.key > x.key ? t->left : t->right);
+        }else{
+            t = new RedBlackNode(x);
+            if(x.key < parent->data.key)
+                parent->left = t;
+            else
+                parent->right = t;
+            insertAdjust(grandP, parent, t);
+            root->colour = BLACK;
+            return ;
+        }
+    }
+}
+
+template <class KEY, class OTHER>
+void RedBlackTree<KEY, OTHER>::insertAdjust(RedBlackNode *gp, RedBlackNode *p, RedBlackNode *t) {
+    if(p->colour)
+        return ;
+    if(p == root){
+        p->colour = BLACK;
+        return ;
+    }
+    if(gp->left == p)
+        if(p->left == t)
+            LL(gp);
+        else
+            LR(gp);
+    else
+        if(p->right == t)
+            RR(gp);
+        else
+            RL(gp);
+}
+
+//删除过程
+template <class KEY, class OTHER>
+void RedBlackTree<KEY, OTHER>::remove(const KEY &x) {
+    KEY del = x;
+    RedBlackNode *p, *t, *c;
+    
+    if(root == NULL)
+        return ;
+    if(root->data.key == x && root->left == NULL && root->right == NULL){
+        delete root;
+        root = NULL;
+        return ;
+    }
+    
+    p = c = t = NULL;
+    while(true){
+        removeAdjust(p, c, t, del);
+        if(c->data.key == del && c->left && c->right){
+            RedBlackNode *tmp = c->right;
+            while(tmp ->left)
+                tmp = tmp->left;
+            c->data = tmp->data;
+            del = tmp->data.key;
+            p = c;
+            c = c->right;
+            t = p->left;
+            continue;
+        }
+        
+        if(c->data.key == del){
+            delete c;
+            if(p->left == c)
+                p->left = NULL;
+            else
+                p->right = NULL;
+            root->colour = BLACK;
+            return ;
+        }
+        
+        p = c;
+        c = (del < p->data.key ? p->left : p->right);
+        t = ( c == p->left ? p->right : p->left);
+    }
+}
+
+template <class KEY, class OTHER>
+void RedBlackTree<KEY, OTHER>::removeAdjust(RedBlackNode *&p, RedBlackNode *&c, RedBlackNode *&t, KEY del) {
+    if(c->colour == RED)
+        return ;
+    if(c == root)
+        if(c->left && c->right && c->right->colour == c->left->colour){
+            c->colour = RED;
+            c->left->colour = c->right->colour = BLACK;
+            return ;
+        }
+    
+    if(((c->left && c->left->colour) || c->left == NULL) && ((c->right && c->right->colour) || c->right == NULL))
+        if(((t->left && t->left->colour) || t->left == NULL) && ((t->right && t->right->colour) || t->right == NULL)){
+            p->colour = BLACK;
+            t->colour = c->colour = RED;
+        }else{
+            if(p->left == t)
+                if(t->left && t->right->colour == RED){
+                    t->left->colour = BLACK;
+                    LL(p);
+                    p = t;
+                }else{
+                    LR(p);
+                    p = p->right;
+                    p->colour = BLACK;
+                }
+            else if(t->right && t->right->colour == RED){
+                t->right->colour = BLACK;
+                RR(p);
+                p = t;
+            }else{
+                RL(p);
+                p = p->left;
+                p->colour = BLACK;
+            }
+            c->colour = RED;
+        }
+        else{
+            if(c ->data.key == del){
+                if(c ->left && c->right){
+                    if(c->right->colour == BLACK){
+                        LL(c);
+                        c = c->right;
+                    }
+                    return ;
+                }
+                if(c->left){
+                    LL(c);
+                    p = c;
+                    c = c->right;
+                }else{
+                    RR(c);
+                    p = c;
+                    c = c->left;
+                }
+            }else{
+                p = c;
+                c = (del < p->data.key ? p->left : p->right);
+                t = ( c == p->left? p->right : p->left);
+                if(c->colour == BLACK){
+                    if(t == p->right)
+                        RR(p);
+                    else
+                        LL(p);
+                    p = t;
+                    t = ( c == p->left ? p->right : p->left);
+                    removeAdjust(p, c, t, del);
+                }
+            }
+        }
+}
+
+
+
 /*
  *********************************************
  使用线性探测法的闭散列表
